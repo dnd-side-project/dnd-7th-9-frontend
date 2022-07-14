fe744e4c98df3698268ffeaeeccb01fe35ec1250
Juhee <juhee@cashwalk.io>
2022-07-14 19:36:47 +0900

    feat: tailwind 셋팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>hello</div>;
+  return (
+    <div className={styles.container}>
+      <p>hello</p>
+      <p className="italic">tailwind TEST</p>
+    </div>
+  );
 };
 
 export default Home;
