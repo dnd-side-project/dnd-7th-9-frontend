d078002ab02c1ba50e725db616e83084fdbca50f
정예원 <yewonn.1j@gmail.com>
2022-07-15 14:05:18 +0900

    style: eslint&prettier 설정

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,13 @@
-import create from "zustand";
+import create from 'zustand';
 
 interface IStore {
-  isDark: boolean;
-  handleIsDark: () => void;
+	isDark: boolean;
+	handleIsDark: () => void;
 }
 
 const useStore = create<IStore>((set) => ({
-  isDark: false,
-  handleIsDark: () => set((state) => ({ isDark: !state.isDark })),
+	isDark: false,
+	handleIsDark: () => set((state) => ({ isDark: !state.isDark })),
 }));
 
 export default useStore;
