807adbd6914b0163de3b6645dc544989bd71b4e2
정예원 <yewonn.1j@gmail.com>
2022-07-21 17:56:31 +0900

    feat : access 토큰 만료시 갱신

---
+++ src/app.modules/api/client.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const client = axios.create({
+	baseURL: '',
+	timeout: 10000,
+	headers: { 'X-Custom-Header': 'foobar' },
+	withCredentials: true,
+});
+
+client.interceptors.response.use(
+	(res) => res,
+	async (error) => {
+		const {
+			config,
+			response: { status },
+		} = error;
+		if (status === 401) {
+			const originalRequest = config;
+			// token refresh 요청
+			const { data } = await axios.post(
+				'' // token refresh api
+			);
+			// 토큰 갱신
+			const { accessToken: newAccessToken } = data;
+			await localStorage.set('TEST_TOKEN', newAccessToken);
+			axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+			originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+			// 401로 요청 실패했던 요청 새로운 accessToken으로 재요청
+			return axios(originalRequest);
+		}
+		return Promise.reject(error);
+	}
+);
+
+export default client;
