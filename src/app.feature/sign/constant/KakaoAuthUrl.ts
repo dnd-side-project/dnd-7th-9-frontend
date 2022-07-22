fc901cddb00cd2ef6ae3f1b6dea2049f821fe3d6
chajuhui123 <chajuhui123@naver.com>
2022-07-22 15:11:05 +0900

    fix: 리다이렉트 URL 환경 변수 적용

--- src/app.feature/sign/constant/KakaoAuthUrl.ts
+++ src/app.feature/sign/constant/KakaoAuthUrl.ts
@@ -1,13 +1,11 @@
-const CLIENT_ID = 'CLIENT_ID';
-
-// TO DO : 서버 환경 / 로컬 환경 도메인 셋팅
-const environment = 'http://localhost:3000';
-const REDIRECT_URI = `${environment}/oauth/kakao`;
+// TO DO : 백 프론트 어디로 리다이렉트 할지 정하기
+const FRONT_REDIRECT_URI = `${process.env.LOCALHOST_URL}/oauth/kakao`;
+const BACK_REDIRECT_URI = `${process.env.API_URL}/login/oauth2/code/kakao`;
 
 // Redirect_URI
 // 프런트엔드 주소로 넣어주면, GET params로 인가 코드가 넘어오고,
 // 백엔드 주소로 넣어주면, GET params로 인가 코드가 넘어온다.
 
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${FRONT_REDIRECT_URI}&response_type=code`;
 
 export default KAKAO_AUTH_URL;
