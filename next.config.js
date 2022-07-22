9b834c136210e01f85e9bc1bf3a14690321c8d9f
chajuhui123 <chajuhui123@naver.com>
2022-07-22 15:10:41 +0900

    docs: 환경 변수 생성

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
 
-module.exports = nextConfig
+	env: {
+		LOCALHOST_URL: 'http://localhost:3000', // 개발 환경
+		KAKAO_CLIENT_ID: 'd7023277c33425eb5d2540210b77250c', // REST API 키
+		API_URL: '',
+	},
+};
+
+module.exports = nextConfig;
