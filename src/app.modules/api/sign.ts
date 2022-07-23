3c37b6b6c0a79cb1280645208c0ca604559c01eb
chajuhui123 <chajuhui123@naver.com>
2022-07-23 13:49:59 +0900

    feat: signIn 로직 작성 (백엔드 서버로 이동시켜줌)

--- src/app.modules/api/sign.ts
+++ src/app.modules/api/sign.ts
@@ -1,4 +1,18 @@
-const sign = () => {
-	return true;
+import axios from 'axios';
+
+const signIn = async (apiUrl: string) => {
+	try {
+		const { data, status } = await axios.post(apiUrl, {
+			headers: {
+				'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+			},
+		});
+		if (status !== 200) throw new Error('ERROR!!');
+		return data;
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
 };
-export default sign;
+
+export default signIn;
