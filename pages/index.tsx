ca266e7ef134eb68e169565b3eb0ca5babd457ff
chajuhui123 <chajuhui123@naver.com>
2022-07-26 11:56:24 +0900

    fix: 백엔드 소셜로그인 화면으로 이동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,25 @@
 import type { NextPage } from 'next';
-import signIn from '../src/app.modules/api/sign';
+import Router from 'next/router';
 import { KAKAO_AUTH_URL, NAVER_AUTH_URL, GOOGLE_AUTH_URL } from '../src/app.feature/sign/constant/SocialAuthUrl';
 
 const Home: NextPage = () => {
-	const handleSocialSignIn = (url: string) => signIn(url);
-
 	return (
 		<div className="w-full">
 			<ul className=" w-1/5 mx-auto">
 				<li className="bg-yellow-400 text-black py-1.5 rounded mt-2">
-					<button type="button" className="w-full relative" onClick={() => handleSocialSignIn(KAKAO_AUTH_URL)}>
+					<button type="button" className="w-full relative" onClick={() => Router.push(KAKAO_AUTH_URL)}>
 						<div className="absolute left-3">K</div>
 						<span className="text-center font-semibold">카카오 로그인</span>
 					</button>
 				</li>
 				<li className="bg-green-500 text-white py-1.5 rounded  mt-2">
-					<button type="button" className="w-full relative" onClick={() => handleSocialSignIn(NAVER_AUTH_URL)}>
+					<button type="button" className="w-full relative" onClick={() => Router.push(NAVER_AUTH_URL)}>
 						<div className="absolute left-3">N</div>
 						<span className="text-center font-semibold">네이버 로그인</span>
 					</button>
 				</li>
 				<li className="bg-red-500 text-white py-1.5 rounded  mt-2">
-					<button type="button" className="w-full relative" onClick={() => handleSocialSignIn(GOOGLE_AUTH_URL)}>
+					<button type="button" className="w-full relative" onClick={() => Router.push(GOOGLE_AUTH_URL)}>
 						<div className="absolute left-3">G</div>
 						<span className="text-center font-semibold">구글 로그인</span>
 					</button>
