2fae0c96deec3f677fa9fb6461bdfc5faff394f4
정예원 <yewonn.1j@gmail.com>
2022-07-28 13:14:06 +0900

    fix : axios 라고 잘못 쓰여진 곳 수정

--- src/app.modules/api/client.ts
+++ src/app.modules/api/client.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const client = axios.create({
-	baseURL: '',
-	timeout: 10000,
-	headers: { 'X-Custom-Header': 'foobar' },
+	baseURL: process.env.API_URL,
 	withCredentials: true,
 });
-
 client.interceptors.response.use(
-	(res) => res,
+	(res) => {
+		return res;
+	},
 	async (error) => {
 		const {
 			config,
@@ -17,16 +16,16 @@ client.interceptors.response.use(
 		if (status === 401) {
 			const originalRequest = config;
 			// token refresh 요청
-			const { data } = await axios.post(
+			const { data } = await client.post(
 				'' // token refresh api
 			);
 			// 토큰 갱신
 			const { accessToken: newAccessToken } = data;
-			localStorage.set('TEST_TOKEN', newAccessToken);
-			axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
+			localStorage.setItem('TEST_TOKEN', newAccessToken);
+			client.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
 			originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 			// 401로 요청 실패했던 요청 새로운 accessToken으로 재요청
-			return axios(originalRequest);
+			return client(originalRequest);
 		}
 		return Promise.reject(error);
 	}
