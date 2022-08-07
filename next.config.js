4d56c290d2e1fa13b08b426d0c5548992fbb11ba
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-08-07 20:56:34 +0900

    build : webpack for svg

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-
+	webpack: (config) => {
+		// 아래를 추가합니다.
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
 	env: {
 		API_URL: 'http://ec2-3-38-222-97.ap-northeast-2.compute.amazonaws.com:8080',
 	},
