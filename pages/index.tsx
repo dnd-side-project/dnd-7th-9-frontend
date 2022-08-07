fbe3bb9d71d018aef81d2b99d2f73aa94d7b32c0
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-08-07 17:39:32 +0900

    style : 폰트 스타일,사이즈,weight 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,24 +9,24 @@ const Home: NextPage = () => {
 				<li className="bg-yellow-400 text-black py-1.5 rounded mt-2">
 					<button type="button" className="w-full relative" onClick={() => Router.push(KAKAO_AUTH_URL)}>
 						<div className="absolute left-3">K</div>
-						<span className="text-center font-semibold">카카오 로그인</span>
+						<span className="text-center font-regular text-xs">카카오 로그인</span>
 					</button>
 				</li>
 				<li className="bg-green-500 text-white py-1.5 rounded  mt-2">
 					<button type="button" className="w-full relative" onClick={() => Router.push(NAVER_AUTH_URL)}>
 						<div className="absolute left-3">N</div>
-						<span className="text-center font-semibold">네이버 로그인</span>
+						<span className="text-center font-medium text-xl">네이버 로그인</span>
 					</button>
 				</li>
 				<li className="bg-red-500 text-white py-1.5 rounded  mt-2">
 					<button type="button" className="w-full relative" onClick={() => Router.push(GOOGLE_AUTH_URL)}>
 						<div className="absolute left-3">G</div>
-						<span className="text-center font-semibold">구글 로그인</span>
+						<span className="text-center font-bold text-2xl">구글 로그인</span>
 					</button>
 				</li>
 				<li className="bg-gray-500 text-white py-1.5 rounded  mt-2">
 					<button type="button" className="w-full relative">
-						<span className="text-center font-semibold">로그아웃</span>
+						<span className="text-center font-bold text-3xl">로그아웃</span>
 					</button>
 				</li>
 			</ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+		fontWeight: {
+			light: 300,
+			regular: 400,
+			medium: 500,
+			bold: 700,
+		},
+		fontSize: {
+			'3xl': '28px',
+			'2xl': '24px',
+			xl: '20px',
+			lg: '18px',
+			base: '16px',
+			tiny: '14px',
+			sm: '12px',
+			xs: '10px',
+		},
+	},
+	plugins: [],
 };
