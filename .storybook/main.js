8daaed1d5057fa6a8ba1f942a99bcd32a699db53
정예원 <yewonn.1j@gmail.com>
2022-08-08 19:10:42 +0900

    chore : style book 설정 for tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,22 @@
+const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'storybook-addon-next-router',
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				postcssLoaderOptions: {
+					implementation: require('postcss'),
+				},
+			},
+		},
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-webpack5',
+	},
+};
