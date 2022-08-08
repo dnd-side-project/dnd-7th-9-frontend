7a8b2301e0440aa9fa178d5c808fc73f0fbecf75
정예원 <yewonn.1j@gmail.com>
2022-08-08 17:31:57 +0900

    chore : 절대 경로&Url 반영

--- src/app.component/navigation/FooterNav.tsx
+++ src/app.component/navigation/FooterNav.tsx
@@ -1,21 +1,22 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import React from 'react';
-import UserIcon from '../../assets/nav/ant-design_user-outlined.svg';
-import HomeIcon from '../../assets/nav/bx_home-alt-2.svg';
-import QuizIcon from '../../assets/nav/material-symbols_quiz-outline.svg';
+import UserIcon from '@assets/nav/ant-design_user-outlined.svg';
+import HomeIcon from '@assets/nav/bx_home-alt-2.svg';
+import QuizIcon from '@assets/nav/material-symbols_quiz-outline.svg';
+import Url from '@app.modules/constant/url';
 
 export default function FooterNav() {
 	const router = useRouter();
 	const fillColor = (path: string) => (router.pathname === path ? '#24cd88' : '#677A90');
 	const Links = [
-		{ path: '/', Icon: <HomeIcon fill={fillColor('/')} />, name: '홈' },
+		{ path: Url.home, Icon: <HomeIcon fill={fillColor(Url.home)} />, name: '홈' },
 		{
-			path: '/my-goal',
-			Icon: <QuizIcon fill={fillColor('/my-goal')} />,
+			path: Url.myGoal,
+			Icon: <QuizIcon fill={fillColor(Url.myGoal)} />,
 			name: 'MY 목표',
 		},
-		{ path: '/mypage', Icon: <UserIcon fill={fillColor('/mypage')} />, name: 'MY' },
+		{ path: Url.mypage, Icon: <UserIcon fill={fillColor(Url.mypage)} />, name: 'MY' },
 	];
 
 	return (
