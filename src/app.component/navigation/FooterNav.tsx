ea3038bc31fe56070d6421e8e338c81df1022100
정예원 <yewonn.1j@gmail.com>
2022-08-10 16:53:36 +0900

    fix: tailwind.config 변경 사항 반영

--- src/app.component/navigation/FooterNav.tsx
+++ src/app.component/navigation/FooterNav.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 import React from 'react';
 import UserIcon from '@assets/nav/ant-design_user-outlined.svg';
@@ -6,31 +5,29 @@ import HomeIcon from '@assets/nav/bx_home-alt-2.svg';
 import QuizIcon from '@assets/nav/material-symbols_quiz-outline.svg';
 import Url from '@app.modules/constant/url';
 
-export default function FooterNav() {
-	const router = useRouter();
-	const fillColor = (path: string) => (router.pathname === path ? '#24cd88' : '#677A90');
+interface Props {
+	curUrl: string;
+}
+export default function FooterNav({ curUrl }: Props) {
+	const fillColor = (iconUrl: string) => (curUrl === iconUrl ? '#24cd88' : '#677A90');
 	const Links = [
-		{ path: Url.home, Icon: <HomeIcon fill={fillColor(Url.home)} />, name: '홈' },
+		{ iconUrl: Url.home, Icon: <HomeIcon fill={fillColor(Url.home)} />, name: '홈' },
 		{
-			path: Url.myGoal,
-			Icon: <QuizIcon fill={fillColor(Url.myGoal)} />,
+			iconUrl: Url.updatedQuizzes,
+			Icon: <QuizIcon fill={fillColor(Url.updatedQuizzes)} />,
 			name: 'MY 목표',
 		},
-		{ path: Url.mypage, Icon: <UserIcon fill={fillColor(Url.mypage)} />, name: 'MY' },
+		{ iconUrl: Url.mypage, Icon: <UserIcon fill={fillColor(Url.mypage)} />, name: 'MY' },
 	];
 
 	return (
-		<div className="fixed bottom-0 w-full h-[77px]  bg-background-white dropShadow-white">
+		<div className="fixed bottom-0 w-full h-[77px]  bg-background-white  drop-shadow-white">
 			<ul className="flex justify-between items-center px-[26px] my-[10px] ">
-				{Links.map(({ path, Icon, name }) => (
-					<Link href={path} key={path}>
+				{Links.map(({ iconUrl, Icon, name }) => (
+					<Link href={iconUrl} key={iconUrl}>
 						<li className="flex flex-col items-center text-primarySlate cursor-pointer">
 							{Icon}
-							<span
-								className={`font-regular text-caption ${
-									router.pathname === path ? 'text-primaryGreen-100' : 'text-primarySlate'
-								}`}
-							>
+							<span className={`font-regular text-caption ${curUrl === iconUrl ? 'text-green-100' : 'text-slate'}`}>
 								{name}
 							</span>
 						</li>
