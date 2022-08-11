6429266de1906822f58d504e5c53cacdecf8382e
chajuhui123 <chajuhui123@naver.com>
2022-08-11 12:25:23 +0900

    feat: 스터디 카테고리 아이콘 리팩토링 및 스토리북 등록

--- src/app.component/icon/CategoryIcon.tsx
+++ src/app.component/icon/CategoryIcon.tsx
@@ -7,29 +7,41 @@ import TestIcon from '@assets/category/test-icon.svg';
 interface Props {
 	className?: string;
 	type: 'certification' | 'company' | 'language' | 'test';
+	colored?: boolean;
 }
 
-export default function CategoryIcon({ className, type }: Props) {
+export default function CategoryIcon({ className, type, colored = true }: Props) {
+	const baseStyle = `inline-block w-[34px] h-[34px] py-2.5 px-2 rounded ${className} `;
 	const categoryInfo = {
 		certification: {
-			icon: <CertificationIcon className="m-auto" width="15px" height="15px" viewBox="0 0 30 30" />,
+			icon: (
+				<section className={`${colored ? 'bg-[#C4DDFF]' : 'bg-background-white'} ${baseStyle}`}>
+					<CertificationIcon className="m-auto" width="15px" height="15px" viewBox="0 0 30 30" />
+				</section>
+			),
 		},
 		company: {
-			icon: <CompanyIcon className="m-auto" width="15px" height="15px" viewBox="0 0 25 25" />,
+			icon: (
+				<section className={`${colored ? 'bg-[#E2EFFF]' : 'bg-background-white'} ${baseStyle}`}>
+					<CompanyIcon className="m-auto" width="15px" height="15px" viewBox="0 0 25 25" />
+				</section>
+			),
 		},
 		language: {
-			icon: <LanguageIcon className="m-auto" width="20px" height="20px" viewBox="0 0 40 40" />,
+			icon: (
+				<section className={`${colored ? 'bg-[#EFF3F6]' : 'bg-background-white'}  ${baseStyle}`}>
+					<LanguageIcon className="m-auto" width="20px" height="20px" viewBox="0 0 40 40" />
+				</section>
+			),
 		},
 		test: {
-			icon: <TestIcon className="m-auto" width="20px" height="20px" viewBox="0 0 20 40" />,
+			icon: (
+				<section className={`${colored ? 'bg-[#F5F7F5]' : 'bg-background-white'} ${baseStyle}`}>
+					<TestIcon className="m-auto" width="20px" height="20px" viewBox="0 0 20 40" />
+				</section>
+			),
 		},
 	};
 
-	const categoryIcon = categoryInfo[type].icon;
-
-	return (
-		<section className={`inline-block w-[34px] h-[34px] py-2.5 rounded bg-background-gray ${className}`}>
-			{categoryIcon}
-		</section>
-	);
+	return categoryInfo[type].icon;
 }
