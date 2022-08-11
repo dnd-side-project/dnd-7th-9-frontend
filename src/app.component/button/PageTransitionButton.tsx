fab012e9f448c84b6bbd9557879068b39aa2a1a6
chajuhui123 <chajuhui123@naver.com>
2022-08-11 11:59:59 +0900

    style: button 관련 img 파일 -> svg 파일로 변경

--- src/app.component/button/PageTransitionButton.tsx
+++ src/app.component/button/PageTransitionButton.tsx
@@ -1,4 +1,8 @@
 import React, { MouseEventHandler } from 'react';
+import LeftArrowAbleIcon from '@assets/button/left-arrow-able.svg';
+import LeftArrowDisableIcon from '@assets/button/left-arrow-disable.svg';
+import RightArrowAbleIcon from '@assets/button/right-arrow-able.svg';
+import RightArrowDisableIcon from '@assets/button/right-arrow-disable.svg';
 
 interface Props {
 	isActive: boolean;
@@ -7,16 +11,35 @@ interface Props {
 }
 
 export default function PageTransitionButton({ isActive, pageTo, onClick, ...props }: Props) {
-	const imgPath = `/img/${pageTo}_${isActive ? 'active' : 'inactive'}.png`;
+	const baseStyle = 'm-auto';
+
+	const getPageTransformIcon = () => {
+		if (isActive) {
+			return pageTo === 'before' ? (
+				<LeftArrowAbleIcon className={baseStyle} />
+			) : (
+				<RightArrowAbleIcon className={baseStyle} />
+			);
+		}
+		return pageTo === 'before' ? (
+			<LeftArrowDisableIcon className={baseStyle} />
+		) : (
+			<RightArrowDisableIcon className={baseStyle} />
+		);
+	};
+
+	const PageTransformIcon = getPageTransformIcon();
 
 	return (
-		<input
-			className="bg-background-white rounded w-[56px] h-[56px] px-5 py-4 drop-shadow-white"
-			type="image"
-			alt=""
-			src={imgPath}
+		<div
+			role="button"
+			className="bg-background-white rounded w-[56px] h-[56px] py-4 drop-shadow-white"
 			onClick={onClick}
+			onKeyPress={undefined}
+			tabIndex={0}
 			{...props}
-		/>
+		>
+			{PageTransformIcon}
+		</div>
 	);
 }
--- src/app.component/button/PlusButton.tsx
+++ src/app.component/button/PlusButton.tsx
@@ -1,4 +1,5 @@
 import React, { MouseEventHandler } from 'react';
+import PlusIcon from '@assets/button/plus.svg';
 
 interface Props {
 	onClick?: MouseEventHandler<HTMLInputElement>;
@@ -6,13 +7,15 @@ interface Props {
 
 export default function PlusButton({ onClick, ...props }: Props) {
 	return (
-		<input
-			className="bg-sub rounded-full w-[75px] h-[75px] p-5 drop-shadow-colored"
-			type="image"
-			alt=""
-			src="/img/plus.png"
+		<div
+			role="button"
+			className="bg-sub rounded-full w-[75px] h-[75px] py-4 drop-shadow-colored"
 			onClick={onClick}
+			onKeyPress={undefined}
+			tabIndex={0}
 			{...props}
-		/>
+		>
+			<PlusIcon className="m-auto" />
+		</div>
 	);
 }
