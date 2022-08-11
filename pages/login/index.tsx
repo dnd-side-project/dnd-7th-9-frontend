cecc030371397a7d371ee8f1a9a03cf05ab40a7e
정예원 <yewonn.1j@gmail.com>
2022-08-11 17:34:16 +0900

    fix : 토큰 redirect 성공 여부 확인 (string이 아니라 boolean타입 반영)

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,21 +1,22 @@
 import type { NextPage } from 'next';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { setCookie } from '@app.modules/cookie/cookie';
 import SignScreen from '@app.feature/sign/screen/SignScreen';
 
 const Sign: NextPage = () => {
+	const router = useRouter();
 	useEffect(() => {
 		const params = new URL(document.location.toString()).searchParams;
-		const isSuccess = params.get('success') === 'success';
-		console.log('success');
+		const isSuccess = params.get('success');
 		if (!isSuccess) return; // TO DO : 로그인 에러 페이지로 이동
 		const accessToken = params.get('token') ?? '';
 		const refreshToken = params.get('refresh') ?? '';
 		localStorage.setItem('TEST_TOKEN', accessToken);
 		setCookie('REFRESH_TOKEN', refreshToken, { path: '/', secure: true, sameSite: 'none' });
-		Router.push('/');
-	}, []);
+
+		router.push('/');
+	}, [router]);
 
 	return <SignScreen />;
 };
