a2ebe745129f2f010362db8825473af698dc8b9a
chajuhui123 <chajuhui123@naver.com>
2022-08-12 21:56:07 +0900

    feat: 기록하기 main 데이터 있는 경우 없는 경우 화면 분리

--- src/app.feature/quiz/screen/UpdatedQuizScreen.tsx
+++ src/app.feature/quiz/screen/UpdatedQuizScreen.tsx
@@ -3,60 +3,35 @@ import PageTitle from '@app.component/title/PageTitle';
 import BellIcon from '@assets/main/eva_bell-outline.svg';
 import UpdatedQuizCard from '../component/card/UpdatedQuizCard';
 
-export default function UpdatedQuizScreen() {
-	const DUMMY_QUIZ_DATA = [
-		{
-			updateTime: 2,
-			userName: '차주희',
-			studyTitle: '프론트 스터디',
-			week: 1,
-		},
-		{
-			updateTime: 3,
-			userName: '정예원',
-			studyTitle: '프론트 스터디',
-			week: 3,
-		},
-		{
-			updateTime: 10,
-			userName: '원태연',
-			studyTitle: '백엔드 스터디',
-			week: 1,
-		},
-		{
-			updateTime: 60,
-			userName: '심미경',
-			studyTitle: '백엔드 스터디',
-			week: 1,
-		},
-		{
-			updateTime: 40000,
-			userName: '최나은',
-			studyTitle: '디자인 스터디',
-			week: 2,
-		},
-		{
-			updateTime: 503000,
-			userName: '송지민',
-			studyTitle: '디자인 스터디',
-			week: 2,
-		},
-	];
+interface IQuiz {
+	updateTime: number;
+	userName: string;
+	studyTitle: string;
+	week: number;
+}
+
+interface Props {
+	quizData: IQuiz[];
+}
+
+export default function UpdatedQuizScreen({ quizData }: Props) {
 	return (
 		<div>
 			<div className="flex justify-between mt-[40px] mb-[55px]">
 				<PageTitle title="실시간 퀴즈" />
 				<BellIcon className="cursor-pointer" />
 			</div>
-			{DUMMY_QUIZ_DATA.map((quiz) => (
-				<UpdatedQuizCard
-					className="mb-[10px]"
-					updateTime={quiz.updateTime}
-					userName={quiz.userName}
-					studyTitle={quiz.studyTitle}
-					week={quiz.week}
-				/>
-			))}
+			<div className="h-full scroll-auto">
+				{quizData.map((quiz) => (
+					<UpdatedQuizCard
+						className="mb-[10px]"
+						updateTime={quiz.updateTime}
+						userName={quiz.userName}
+						studyTitle={quiz.studyTitle}
+						week={quiz.week}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
