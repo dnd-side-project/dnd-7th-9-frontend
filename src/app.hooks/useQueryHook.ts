b35aa380ffe294ed6f8f2b553e8fb290fa6b118c
chajuhui123 <chajuhui123@naver.com>
2022-08-15 01:38:59 +0900

    refactor: useQueryHook accessToken 관련 코드 수정

--- src/app.hooks/useQueryHook.ts
+++ src/app.hooks/useQueryHook.ts
@@ -1,16 +1,14 @@
 import { useQuery } from 'react-query';
 import client from '@app.modules/api/client';
-import { getCookie } from '@app.modules/cookie/cookie';
 
 interface IRequest {
 	url: string;
 	data: object;
-	accessToken: string;
 }
 
-const requestFn = async ({ url, data, accessToken }: IRequest) => {
+const requestFn = async ({ url, data }: IRequest) => {
 	const response = await client.get(url, {
-		data: { ...data, accessToken },
+		data: { ...data },
 	});
 
 	if (response.data.error) throw response.data.error;
@@ -30,7 +28,6 @@ const getQueryKeyFn = (queryKeys: [string, object | any] | string) => {
 // const { data, isLoading, refetch ...} = useQueryHook("API_URL", ...); // API URL만 있는 경우
 // const { data, isLoading, refetch ...} = useQueryHook(["API_URL", { data }], ...); // 파라미터 있는 경우
 const useQueryHook = (queryKeys: [string, object | any] | string, options = {}) => {
-	const accessToken = getCookie('TEST_TOKEN');
 	const { url, data } = getQueryKeyFn(queryKeys);
 
 	return useQuery(
@@ -39,7 +36,6 @@ const useQueryHook = (queryKeys: [string, object | any] | string, options = {}) 
 			requestFn({
 				url,
 				data,
-				accessToken,
 			}),
 		{
 			enabled: !!queryKeys,
