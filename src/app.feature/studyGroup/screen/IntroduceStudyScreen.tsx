2c7cdb7f282fe9dc648fd045f3d55754644a73fa
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2022-08-18 01:53:13 +0900

    feat : CreateStudyLayout

--- src/app.feature/studyGroup/screen/IntroduceStudyScreen.tsx
+++ src/app.feature/studyGroup/screen/IntroduceStudyScreen.tsx
@@ -1,22 +1,11 @@
-import { useRouter } from 'next/router';
-
-import ProgressHeader from '@app.component/header/Progress';
-import PageController from '@app.component/pageController/PageController';
-
-import BackAlertModal from '@app.component/modal/BackAlertModal';
 import CompanyIcon from '@assets/study/company_icon.svg';
 import LanguageIcon from '@assets/study/language_icon.svg';
 import CertificationIcon from '@assets/study/certification_icon.svg';
 import ExamIcon from '@assets/study/Exam_icon.svg';
-
-import { useState } from 'react';
-
 import Box from '@app.component/box';
+import CreateStudyLayout from '@app.feature/studyGroup/component/Layout';
 import RequireMark from '../component/RequireMark';
 
-interface Props {
-	submitHandler: () => void;
-}
 export const STUDY_CATEGORIES = [
 	{
 		category: '취업',
@@ -36,31 +25,13 @@ export const STUDY_CATEGORIES = [
 	},
 ];
 
-export default function IntroduceStudyScreenScreen({ submitHandler }: Props) {
-	const router = useRouter();
-	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
+export default function IntroduceStudyScreenScreen() {
 	const CUR_PAGE = 1;
-	const STEPS_COUNT = 3;
-	const toPrevHandler = () => {
-		// if (CUR_PAGE <= 1) return;
-		// router.push(`/create-study/${pageIdx}`);
-	};
-
-	const toNextHandler = () => {
-		if (CUR_PAGE >= STEPS_COUNT) return;
-		router.push(`/create-study/${CUR_PAGE + 1}`);
-	};
 
 	return (
-		<div>
-			<ProgressHeader curPage={CUR_PAGE} pagesLength={STEPS_COUNT} backAlertModalOpen={() => setIsModalOpen(true)} />
-			<div className="mt-[64px] mb-[120.07px]">
-				<div className="flex justify-between items-center">
-					<span className="font-bold text-title2 text-black-400">당신의 스터디를 소개해 주세요</span>
-					<RequireMark />
-				</div>
-				<ul className="flex justify-between items-center mt-[30px]">
+		<CreateStudyLayout curPage={CUR_PAGE} todo="당신의 스터디를 소개해 주세요">
+			<>
+				<ul className="flex justify-between items-center ">
 					{STUDY_CATEGORIES.map((study) => (
 						<li key={study.category} className="flex flex-col items-center space-y-[8.61px]">
 							<button type="button" className="bg-background-white rounded w-[72px] h-[72px] grid place-items-center">
@@ -75,16 +46,7 @@ export default function IntroduceStudyScreenScreen({ submitHandler }: Props) {
 					<input placeholder="스터디 그룹명을 적어주세요." className="w-full  outline-none text-body1 font-medium" />
 					<RequireMark className="absolute right-[16.05px] top-[13.98px]" />
 				</Box>
-				<BackAlertModal isModalOpen={isModalOpen} onCloseModal={() => setIsModalOpen(false)} />
-			</div>
-			<PageController
-				curPage={CUR_PAGE}
-				pagesLength={STEPS_COUNT}
-				toPrevHandler={toPrevHandler}
-				toNextHandler={toNextHandler}
-				finishHandler={submitHandler}
-				finishWord="끝내기"
-			/>
-		</div>
+			</>
+		</CreateStudyLayout>
 	);
 }
--- src/app.feature/studyGroup/screen/StudyGoalScreen.tsx
+++ src/app.feature/studyGroup/screen/StudyGoalScreen.tsx
@@ -1,58 +1,20 @@
-import { useRouter } from 'next/router';
-
-import ProgressHeader from '@app.component/header/Progress';
-import PageController from '@app.component/pageController/PageController';
-
-import BackAlertModal from '@app.component/modal/BackAlertModal';
-
-import { useState } from 'react';
-
 import Box from '@app.component/box';
+import CreateStudyLayout from '@app.feature/studyGroup/component/Layout';
 import RequireMark from '../component/RequireMark';
 
-interface Props {
-	submitHandler: () => void;
-}
-
-export default function StudyGoalScreen({ submitHandler }: Props) {
-	const router = useRouter();
-	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
+export default function StudyGoalScreen() {
 	const CUR_PAGE = 3;
-	const STEPS_COUNT = 3;
-	const toPrevHandler = () => {
-		if (CUR_PAGE <= 1) return;
-		router.push(`/create-study/${CUR_PAGE - 1}`);
-	};
-
-	const toNextHandler = () => {
-		if (CUR_PAGE >= STEPS_COUNT) return;
-		router.push(`/create-study/${CUR_PAGE + 1}`);
-	};
 
 	return (
-		<div>
-			<ProgressHeader curPage={CUR_PAGE} pagesLength={STEPS_COUNT} backAlertModalOpen={() => setIsModalOpen(true)} />
-			<div className="mt-[64px] mb-[120.07px]">
-				<span className="font-bold text-title2 text-black-400">스터디 그룹의 목표를 주세요</span>
-
-				<span className="block mt-[42.39px]  text-slate text-small2 font-bold">최종 목표</span>
+		<CreateStudyLayout curPage={CUR_PAGE} todo="스터디 그룹의 목표를 적어 주세요">
+			<>
+				<span className="block mt-[12.39px]  text-slate text-small2 font-bold">최종 목표</span>
 
 				<Box height="h-[88px]" className="relative px-[22.09px]  mt-[7.63px]">
 					<input placeholder="최종 목표를 적어주세요." className="w-full  outline-none text-body1 font-medium" />
 					<RequireMark className="absolute right-[16.05px] top-[13.98px]" />
 				</Box>
-
-				<BackAlertModal isModalOpen={isModalOpen} onCloseModal={() => setIsModalOpen(false)} />
-			</div>
-			<PageController
-				curPage={CUR_PAGE}
-				pagesLength={5}
-				toPrevHandler={toPrevHandler}
-				toNextHandler={toNextHandler}
-				finishHandler={submitHandler}
-				finishWord="끝내기"
-			/>
-		</div>
+			</>
+		</CreateStudyLayout>
 	);
 }
