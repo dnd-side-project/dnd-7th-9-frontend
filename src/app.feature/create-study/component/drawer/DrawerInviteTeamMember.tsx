815bb07abc6fdb91727a3ca785fddfdb7b609fe6
chajuhui123 <chajuhui123@naver.com>
2022-08-17 18:39:32 +0900

    feat: 스터디원 초대 Drawer 컴포넌트 생성

--- src/app.feature/create-study/component/drawer/DrawerInviteTeamMember.tsx
+++ src/app.feature/create-study/component/drawer/DrawerInviteTeamMember.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Input from '@app.component/input';
 import DefaultButton from '@app.component/button/DefaultButton';
 import ItemInvitedMember from '../item/ItemInvitedMember';
 
 export default function DrawerInviteTeamMember() {
+	const router = useRouter();
+
 	const [isInviteDrawerOpen, setIsInviteDrawerOpen] = useState(false);
 	const [inputMember, setInputMember] = useState('');
 	const [invitedTeamMember, setInvitedTeamMember] = useState<string[]>([]);
@@ -13,8 +16,13 @@ export default function DrawerInviteTeamMember() {
 	};
 
 	const handleInviteMember = () => {
+		if (inputMember.trim() === '' || invitedTeamMember.includes(inputMember)) return; // TO DO : 이미 목록에 있거나 유효하지 않은 유저에 대한 처리
 		setInvitedTeamMember((prev) => [...prev, inputMember]);
 		setInputMember('');
+	};
+
+	const handleOnDelete = (deleteIndex: number) => {
+		setInvitedTeamMember(invitedTeamMember.filter((_, index) => index !== deleteIndex));
 	};
 
 	return (
@@ -38,13 +46,14 @@ export default function DrawerInviteTeamMember() {
 			</div>
 			{isInviteDrawerOpen && (
 				<div className="overflow-auto h-[350px] mb-[10px] px-[-10px]">
-					{invitedTeamMember.map((member) => (
-						<ItemInvitedMember member={member} />
+					{invitedTeamMember.map((member, index) => (
+						<ItemInvitedMember key={member} member={member} onDelete={() => handleOnDelete(index)} />
 					))}
 				</div>
 			)}
 
-			<DefaultButton text="초대하기" />
+			{/* TO DO : 스터디 그룹 생성 & 사용자 초대 API 연동 */}
+			<DefaultButton text="초대하기" onClick={() => router.push('/complete/invite-member')} />
 		</div>
 	);
 }
