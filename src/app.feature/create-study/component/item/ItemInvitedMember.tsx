9aacd07f63d63ffa7ec51af998a75dce87af2655
chajuhui123 <chajuhui123@naver.com>
2022-08-17 18:39:47 +0900

    feat: 초대할 스터디 맴버 이메일 컴포넌트 생성

--- src/app.feature/create-study/component/item/ItemInvitedMember.tsx
+++ src/app.feature/create-study/component/item/ItemInvitedMember.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import DeleteIcon from '@assets/create-study/cancel.svg';
 
 interface Props {
 	member: string;
-	onDelete?: void;
+	onDelete?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function ItemInvitedMember({ member, onDelete }: Props) {
 	return (
 		<div className="flex justify-between bg-background-gray shadow-white rounded mx-[20px] mb-[18px] pl-[18px]">
 			<div className="py-[19px]">{member}</div>
-			<div className="pt-[6px] pr-[4px]">
-				<DeleteIcon onClick={onDelete} />
+			<div className="pt-[6px] pr-[4px]" role="button">
+				<button type="button" onClick={onDelete}>
+					<DeleteIcon />
+				</button>
 			</div>
 		</div>
 	);
