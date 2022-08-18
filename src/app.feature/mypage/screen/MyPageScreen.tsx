98b443825cde88ec65679d5f0c7c2b6e2b98d816
chajuhui123 <chajuhui123@naver.com>
2022-08-18 23:03:43 +0900

    feat: router query 기반 myPage 필터링 기능 생성

--- src/app.feature/mypage/screen/MyPageScreen.tsx
+++ src/app.feature/mypage/screen/MyPageScreen.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
+import Router, { useRouter } from 'next/router';
+import Accordion from '@app.component/accordion';
+import CategoryIcon from '@app.component/icon/CategoryIcon';
+import MyPageUserProfile from '../component/profile/MyPageUserProfile';
+import MyPageStudyCategory from '../component/category/MyPageStudyCategory';
+import MyPageStudyStatusToggle from '../component/toggle/MyPageStudyStatusToggle';
+import MyPageStudyAccordionContent from '../component/accordian-content/MyPageStudyAccordionContent';
+
+interface IFilteringInput {
+	[key: string]: string;
+}
 
 export default function MyPageScreen() {
-	return <div>MyPageScreen</div>;
+	const router = useRouter();
+	const { toggle = 'proceeding', category = 'all' } = router.query;
+
+	const handleFilteringMyPage = (input: IFilteringInput) => {
+		Router.push({
+			pathname: '/mypage',
+			query: { ...router.query, ...input },
+		});
+	};
+
+	console.log(router, router.query, toggle, category);
+	return (
+		<div>
+			{/* 프로필 영역 */}
+			<MyPageUserProfile />
+
+			{/* 토글 영역 */}
+			<MyPageStudyStatusToggle toggle={toggle} handleFilteringMyPage={handleFilteringMyPage} />
+
+			{/* 스터디 카테고리 영역 */}
+			<MyPageStudyCategory />
+
+			{/* 스터디 리스트 영역 */}
+			<section>
+				<div>
+					<Accordion
+						className="mb-[10px]"
+						icon={<CategoryIcon type="language" />}
+						text="중국어 스터디"
+						status="완료"
+						content={<MyPageStudyAccordionContent />}
+					/>
+				</div>
+			</section>
+		</div>
+	);
 }
