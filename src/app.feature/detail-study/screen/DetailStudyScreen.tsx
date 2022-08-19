d5ed7aeae38fc0f6f1ba56fd8fb03579936df774
정예원 <yewonn.1j@gmail.com>
2022-08-19 14:04:26 +0900

    feat : 세부 목표 overflow-x, 세부 목표 텍스트 길어짐 임시 처리

--- src/app.feature/detail-study/screen/DetailStudyScreen.tsx
+++ src/app.feature/detail-study/screen/DetailStudyScreen.tsx
@@ -1,91 +1,67 @@
 import { useRouter } from 'next/router';
 
-import AnswerCheckButton from '@app.feature/quiz/component/button/AnswerCheckButton';
-import useSolveQuizStore from '@app.modules/store/quiz/solveQuiz'; // temp
-import PageController from '@app.component/pageController/PageController';
-import ProgressHeader from '@app.component/header/Progress';
-import { useState } from 'react';
-import BackAlertModal from '@app.component/modal/BackAlertModal';
-import Box from '@app.component/box';
-import GoBackHeader from '@app.component/header/GoBack';
 import DetailStudyHeader from '@app.feature/detail-study/component/header';
 import PlusIcon from '@assets/study/plus_icon.svg';
-import QuizCompleteGraph from '@app.feature/complete/component/graph/QuizCompleteGraph';
-import PencilIcon from '@assets/complete/pencil.svg';
+
 import 'chart.js/auto';
 import { Doughnut } from 'react-chartjs-2';
 import PersonIcon from '@assets/complete/person.svg';
 import RunningPersonIcon from '@assets/complete/running-person.svg';
-import QuizCompleteGraphScore from '@app.feature/complete/component/graph/QuizCompleteGraphScore';
+
 import DocumentIcon from '@assets/study/fluent_document-edit-20-regular.svg';
-// import QuizCompleteGraphScore from './QuizCompleteGraphScore';
+
 import ArrowBottomIcon from '@assets/main/arrow-bottom.svg';
 import ArrowTopIcon from '@assets/main/arrow-top.svg';
+import { useState } from 'react';
 
 interface Props {
 	studyId: string;
 }
+const DUMMY_DATA1 = {
+	score: 80,
+	solveScore: 30, // 기존 문제 해결 점수
+	createScore: 0, // 기존 문제 작성 점수
+	addPercentageCreate: 50, // 증가한 목표달성율 퍼센티지 (문제 출제 ver) => 0 혹은 50
+	addPercentageSolve: 0, // 증가한 목표달성율 퍼센티지 (문제 풀기 ver)
+	correctCount: 0, // 사용자가 맞춘 갯수
+	totalCount: 5, // 문제집 문제 전체 갯수
+};
+const chartCommonStyle = {
+	cutout: '85%',
+	borderRadius: 100,
+	borderWidth: [0, 1, 0],
+};
 
-export default function DetailStudyScreen({ studyId }: Props) {
-	const DUMMY_DATA1 = {
-		score: 80,
-		solveScore: 30, // 기존 문제 해결 점수
-		createScore: 0, // 기존 문제 작성 점수
-		addPercentageCreate: 50, // 증가한 목표달성율 퍼센티지 (문제 출제 ver) => 0 혹은 50
-		addPercentageSolve: 0, // 증가한 목표달성율 퍼센티지 (문제 풀기 ver)
-		correctCount: 0, // 사용자가 맞춘 갯수
-		totalCount: 5, // 문제집 문제 전체 갯수
-	};
-	const chartCommonStyle = {
-		cutout: '85%',
-		borderRadius: 100,
-		borderWidth: [0, 1, 0],
-	};
-
-	const quizCreateChartData = {
-		labels: ['quiz-create'],
-		datasets: [
-			{
-				label: 'quizCreate',
-				borderColor: ['rgba( 255, 255, 255, 0)', '#02CABF', 'rgba( 255, 255, 255, 0)'],
-				backgroundColor: ['rgba( 255, 255, 255, 0)', 'rgba( 255, 255, 255, 0 )', '#01AEB7'],
-				data: [
-					50,
-					50 - DUMMY_DATA1.addPercentageCreate - DUMMY_DATA1.createScore,
-					DUMMY_DATA1.addPercentageCreate + DUMMY_DATA1.createScore,
-				],
-				...chartCommonStyle,
-			},
-		],
-	};
-
-	const quizSolveChartData = {
-		labels: ['quiz-solve'],
-		datasets: [
-			{
-				label: 'quizSolve',
-				borderColor: ['rgba( 255, 255, 255, 0)', '#02CABF', 'rgba( 255, 255, 255, 0)'],
-				backgroundColor: ['#93F358', 'rgba( 255, 255, 255, 0)', 'rgba( 255, 255, 255, 0 )'],
-				data: [
-					DUMMY_DATA1.addPercentageSolve + DUMMY_DATA1.solveScore,
-					50 - DUMMY_DATA1.addPercentageSolve - DUMMY_DATA1.solveScore,
-					50,
-				],
-				...chartCommonStyle,
-			},
-		],
-	};
-
-	const chartOptions = {
-		plugins: {
-			legend: {
-				display: false,
-			},
-			tooltip: {
-				enabled: false,
-			},
+const quizCreateChartData = {
+	labels: ['quiz-create'],
+	datasets: [
+		{
+			label: 'quizCreate',
+			borderColor: ['rgba( 255, 255, 255, 0)', '#02CABF', 'rgba( 255, 255, 255, 0)'],
+			backgroundColor: ['rgba( 255, 255, 255, 0)', 'rgba( 255, 255, 255, 0 )', '#01AEB7'],
+			data: [
+				50,
+				50 - DUMMY_DATA1.addPercentageCreate - DUMMY_DATA1.createScore,
+				DUMMY_DATA1.addPercentageCreate + DUMMY_DATA1.createScore,
+			],
+			...chartCommonStyle,
 		},
-	};
+	],
+};
+
+const chartOptions = {
+	plugins: {
+		legend: {
+			display: false,
+		},
+		tooltip: {
+			enabled: false,
+		},
+	},
+};
+export default function DetailStudyScreen({ studyId }: Props) {
+	const router = useRouter();
+	const [curGoalId, setCurGoalId] = useState<number>(0); // temp
 	return (
 		<div>
 			<DetailStudyHeader />
@@ -94,19 +70,38 @@ export default function DetailStudyScreen({ studyId }: Props) {
 					<span className="text-body2 text-black-400 font-medium ">전체 목표</span>
 					<span className=" text-body2 text-slate font-medium ">오픽 AL 달성</span>
 				</div>
-				<div className="mt-[19.87px] pt-[36.72px] bg-background-white -mx-[20px] px-[20px] pb-[29.08px] ">
+				<div className="mt-[19.87px]  pt-[36.72px] bg-background-white -mx-[20px] px-[20px] pb-[29.08px] ">
 					<span className="text-black-500 font-medium text-body2">세부 목표</span>
-					<div className="mt-[22px] flex items-center space-x-[12.2px]">
-						<div className="rounded bg-slate p-[12.2px] flex flex-col space-y-[9.4px] w-[135.2px]">
-							<span className="text-white text-body2 font-medium">집 문제 예상 답변 준비하기</span>
-							<span className="text-white text-caption font-regular">8/31-9/2</span>
-							<span className="text-white text-caption font-bold">목표 달성률 90%</span>
-						</div>
-
-						<div className="rounded bg-[#EEEEEE] p-[12.2px] flex flex-col space-y-[9.4px] w-[135.2px]">
-							<span className="text-slate text-body2 font-medium">집 문제 예상 답변 준비하기</span>
-							<span className="text-slate text-caption font-regular">8/31-9/2</span>
-							<span className="text-slate text-caption font-bold">목표 달성률 90%</span>
+					<div className="mt-[22px] flex items-center space-x-[12.2px] overflow-x-auto ">
+						{[1, 1, 1].map((_, index) => (
+							<button
+								type="button"
+								onClick={() => setCurGoalId(index)}
+								className={`rounded ${
+									curGoalId === index ? 'bg-slate' : 'bg-[#EEEEEE]'
+								} p-[12.2px] flex flex-col items-start w-[135.2px] min-w-[135.2px]  min-h-[135.2px] `}
+							>
+								<span
+									className={`${
+										curGoalId === index ? 'text-white' : 'text-slate'
+									} block h-[48px] text-white text-body2 overflow-y-auto font-medium text-start  `}
+								>
+									집 문제 예상 답변 준비하기
+								</span>
+								<span
+									className={`${
+										curGoalId === index ? 'text-white' : 'text-slate'
+									} block my-[9.4px] text-caption font-regular`}
+								>
+									8/31-9/2
+								</span>
+								<span className={`${curGoalId === index ? 'text-white' : 'text-slate'} text-caption font-bold`}>
+									목표 달성률 90%
+								</span>
+							</button>
+						))}
+						<div className="rounded bg-[#EEEEEE]  grid place-items-center w-[135.2px]  min-w-[135.2px] h-[135.2px]  min-h-[135.2px]">
+							<PlusIcon />
 						</div>
 					</div>
 				</div>
