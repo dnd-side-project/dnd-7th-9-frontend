ce7374b52fb9e2cafcd4c48fd83189c4d0b22f61
chajuhui123 <chajuhui123@naver.com>
2022-08-19 18:45:43 +0900

    fix: input 공통 컴포넌트 필수값인 경우 props 추가

--- src/app.component/input/index.tsx
+++ src/app.component/input/index.tsx
@@ -1,6 +1,8 @@
+import RequireMark from '@app.component/requireMark/RequireMark';
 import React, { ChangeEvent, MouseEvent } from 'react';
 
 interface Props {
+	required?: boolean;
 	className?: string;
 	placeholder?: string;
 	value?: string;
@@ -8,6 +10,14 @@ interface Props {
 	onClick?: (e: MouseEvent<HTMLElement>) => void;
 }
 
-export default function Input({ className, ...props }: Props) {
-	return <input className={`drop-shadow-white placeholder:text-gray py-[19px] px-[18px] ${className}`} {...props} />;
+export default function Input({ className, required = false, ...props }: Props) {
+	return (
+		<div>
+			{required && <RequireMark className="absolute z-10 m-[10px] right-[20px]" />}
+			<input
+				className={`relative drop-shadow-white placeholder:text-gray py-[19px] px-[18px] ${className}`}
+				{...props}
+			/>
+		</div>
+	);
 }
