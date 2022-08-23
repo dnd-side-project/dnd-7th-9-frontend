ad564df4b9cfb90bef8cefe5ab27ed359ee6ddf5
chajuhui123 <chajuhui123@naver.com>
2022-08-23 15:24:22 +0900

    fix: 로그인 토큰 헤더 관련 로직 수정

--- src/app.modules/api/client.ts
+++ src/app.modules/api/client.ts
@@ -1,9 +1,28 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
+import { getCookie, setCookie } from '@app.modules/cookie/cookie';
 
 const client = axios.create({
 	baseURL: process.env.API_URL,
-	withCredentials: true,
+	// TO DO : 임시로 CORS 에러 방지해놓음. 백엔드와 맞춰봐야 함.
+	withCredentials: false,
+	headers: {
+		'Access-Control-Allow-Origin': 'http://localhost:3000/',
+	},
 });
+
+client.interceptors.request.use(
+	(config) => {
+		const token = localStorage.getItem('TEST_TOKEN');
+
+		if (token) client.defaults.headers.common['Access-Token'] = token;
+
+		return config;
+	},
+	(error) => {
+		Promise.reject(error);
+	}
+);
+
 client.interceptors.response.use(
 	(res) => {
 		return res;
@@ -13,22 +32,31 @@ client.interceptors.response.use(
 			config,
 			response: { status },
 		} = error;
-		if (status === 401) {
+
+		if (status === 400) {
 			try {
 				const originalRequest = config;
+
 				// token refresh 요청
-				const { data } = await client.post(
-					'/auth/token/reissue' // token refresh api
+				const refreshToken = getCookie('REFRESH_TOKEN');
+				const { data } = await client.get(
+					'/auth/token/reissue', // token refresh api
+					{ headers: { 'Refresh-Token': refreshToken } }
 				);
+
 				// 토큰 갱신
-				const { accessToken: newAccessToken } = data;
+				const { accessToken: newAccessToken, refreshToken: newRefreshToken } = data;
+
 				localStorage.setItem('TEST_TOKEN', newAccessToken);
-				client.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-				originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+				setCookie('REFRESH_TOKEN', newRefreshToken, { path: '/', secure: true, sameSite: 'none' });
+
+				client.defaults.headers.common['Access-Token'] = newAccessToken;
+				originalRequest.headers['Access-Token'] = newAccessToken;
+
 				// 401로 요청 실패했던 요청 새로운 accessToken으로 재요청
 				return client(originalRequest);
 			} catch (refreshError) {
-				return Promise.reject(refreshError); // refresh token 문제로 access 토큰 갱신 실패, 로그인화면으로 이동
+				return null; // refresh token 문제로 access 토큰 갱신 실패, 로그인화면으로 이동
 			}
 		}
 		return Promise.reject(error);
