a9d5b02be331f29df3dd80c3ebc6c3bceeb5fbdc
chajuhui123 <chajuhui123@naver.com>
2022-09-05 14:04:49 +0900

    fix: 타입관련 이슈 수정

--- src/app.feature/complete/component/graph/QuizCompleteGraph.stories.tsx
+++ src/app.feature/complete/component/graph/QuizCompleteGraph.stories.tsx
@@ -11,11 +11,8 @@ const Template: ComponentStory<typeof QuizCompleteGraph> = (args) => <QuizComple
 
 export const Default = Template.bind({});
 Default.args = {
-	score: 90, // 유저의 이번주 목표 점수 (최종 점수)
-	createScore: 50,
-	solveScore: 10,
-	addPercentageCreate: 0, // 증가한 목표달성율 퍼센티지 (문제 출제 ver) => 0 혹은 50
-	addPercentageSolve: 30, // 증가한 목표달성율 퍼센티지 (문제 풀기 ver)
-	correctCount: 5, // 사용자가 맞춘 갯수
-	totalCount: 5, // 문제집 문제 전체 갯수
+	addedRate: 30, // 추가된 달성률
+	userTotalRate: 90, // 현재 세부 목표에 대한 총 달성률
+	questionBookPostRate: 50, // 문제 출제로 부터 얻은 달성률
+	questionBookSolveRate: 10, // 문제 풀이로 부터 얻은 달성률
 };
