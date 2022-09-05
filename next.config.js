5577fc80bfdb7ce4eb6bbfcb21877a7d4bd3c8a4
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2022-09-06 05:58:36 +0900

    fix : 백엔드 서버 주소 변경

--- next.config.js
+++ next.config.js
@@ -26,9 +26,8 @@ const nextConfig = {
 		return config;
 	},
 
-
 	env: {
-		API_URL: 'http://ec2-3-38-222-97.ap-northeast-2.compute.amazonaws.com:8080',
+		API_URL: 'http://ec2-15-165-255-15.ap-northeast-2.compute.amazonaws.com:8080',
 	},
 };
 
