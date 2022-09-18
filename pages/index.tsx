534a6448935943e18580d5903700bb81822caa25
chajuhui123 <chajuhui123@naver.com>
2022-09-18 17:22:12 +0900

    feat: 그룹/세부 목표 조회 useQuery 문 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,34 @@
 import type { NextPage } from 'next';
-
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import HomeScreen from '@app.feature/home/screen/HomeScreen';
 import InitHomeScreen from '@app.feature/home/screen/InitHomeScreen';
+import { useQuery } from '@tanstack/react-query';
+import fetchGetUserStudyList from '@app.feature/home/api';
 
 const Home: NextPage = () => {
-	return <div>{false ? <HomeScreen /> : <InitHomeScreen />}</div>;
+	const router = useRouter();
+
+	const [userStudyList, setUserStudyList] = useState([]);
+
+	const query = useQuery(['user', 'list'], () => fetchGetUserStudyList(), {
+		retry: 1,
+		onSuccess: (data) => {
+			console.log(data.data.result);
+			setUserStudyList(data.data.result);
+		},
+		onError: () => {
+			alert('알 수 없는 에러가 발생했습니다.');
+			// router.push('/');
+		},
+	});
+
+	useEffect(() => {
+		const accessToken = typeof window !== 'undefined' ? localStorage.getItem('TEST_TOKEN') : null;
+		if (!accessToken) router.push('/login');
+	}, []);
+
+	return <div>{true ? <HomeScreen userStudyList={userStudyList} /> : <InitHomeScreen />}</div>;
 };
 
 export default Home;
