0f01618e1877dd3a352116e9a20709c2cc6521e6
chajuhui123 <chajuhui123@naver.com>
2022-10-03 00:16:02 +0900

    fix: 더미데이터

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,43 @@ import InitHomeScreen from '@app.feature/home/screen/InitHomeScreen';
 import fetchGetUserStudyList from '@app.feature/home/api';
 
 const Home: NextPage = () => {
-	const [userStudyList, setUserStudyList] = useState([]);
+	const [userStudyList, setUserStudyList] = useState([
+		{
+			studyGroupListResponse: {
+				groupId: 1,
+				groupName: 'DND 짱들',
+				groupStartDate: '2022-08-15',
+				groupEndDate: '2022-12-15',
+				groupGoal: '사이드 프로젝트',
+				groupImageUrl: null,
+				groupCategory: 'EMPLOYMENT',
+				groupStatus: 'ACTIVE',
+			},
+			activeGoalResponse: {
+				goalContent: 'DND 프로젝트 디벨롭 회의 및 정리',
+				goalStartDate: '2022-09-07',
+				goalEndDate: '2022-09-30',
+				goalStatus: 'ACTIVE',
+				achieveGoalStatus: false,
+				checkSubmitQuestionBook: false,
+				checkSolveQuestionBook: true,
+				clearSolveQuestionBookNum: 0,
+				toSolveQuestionBookNum: 0,
+				questionPerQuestionBook: 2, // 추가 (문제 생성 시, 세부 목표에 저장되어 있는 문제 개수 정보 필요)
+			},
+		},
+	]);
 
-	const query = useQuery(['user', 'list'], () => fetchGetUserStudyList(), {
-		retry: 1,
-		onSuccess: (data) => {
-			console.log(data.data.result);
-			setUserStudyList(data.data.result);
-		},
-		onError: () => {
-			alert('알 수 없는 에러가 발생했습니다.');
-		},
-	});
+	// const query = useQuery(['user', 'list'], () => fetchGetUserStudyList(), {
+	// 	retry: 1,
+	// 	onSuccess: (data) => {
+	// 		console.log(data.data.result);
+	// 		setUserStudyList(data.data.result);
+	// 	},
+	// 	onError: () => {
+	// 		alert('알 수 없는 에러가 발생했습니다.');
+	// 	},
+	// });
 
 	return <div>{userStudyList.length ? <HomeScreen userStudyList={userStudyList} /> : <InitHomeScreen />}</div>;
 };
--- src/app.feature/home/component/accordionContent/StudyGoalAccordionContent.tsx
+++ src/app.feature/home/component/accordionContent/StudyGoalAccordionContent.tsx
@@ -4,20 +4,21 @@ import { useRouter } from 'next/router';
 
 interface Props {
 	groupId: number;
+	studyId?: number;
 }
 
-export default function StudyGoalAccordionContent({ groupId }: Props) {
+export default function StudyGoalAccordionContent({ groupId, studyId }: Props) {
 	const router = useRouter();
 	return (
 		<div>
 			<div className="grid grid-cols-2 gap-2">
 				<DefaultButton
 					className="mt-3"
-					// TO DO : 문제집 번호로 연결
-					// onClick={() => router.push(``)}
+					// TO DO : 스터디 번호로 연결
+					onClick={() => router.push(`/studies/${studyId}`)}
 					text="문제집 보러가기"
 				/>
-				{/* goalId 함께 넘겨주기 */}
+				{/* TO DO : goal Id 로 연결 */}
 				<DefaultButton className="mt-3" onClick={() => router.push(`/create-quiz/${groupId}`)} text="문제 작성하기" />
 			</div>
 		</div>
