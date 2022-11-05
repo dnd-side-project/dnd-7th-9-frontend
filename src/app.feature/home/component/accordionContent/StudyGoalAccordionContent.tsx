63610f1b3370706ea2135c59e8cd332c36acd007
chajuhui123 <chajuhui123@naver.com>
2022-11-05 20:50:05 +0900

    feat: 공통 버튼 complete type 추가

--- src/app.feature/home/component/accordionContent/StudyGoalAccordionContent.tsx
+++ src/app.feature/home/component/accordionContent/StudyGoalAccordionContent.tsx
@@ -3,23 +3,23 @@ import DefaultButton from '@app.component/button/DefaultButton';
 import { useRouter } from 'next/router';
 
 interface Props {
-	groupId: number;
-	studyId?: number;
+	goalId: number;
+	studyId: number;
+	questionBookSubmitted: boolean;
 }
 
-export default function StudyGoalAccordionContent({ groupId, studyId }: Props) {
+export default function StudyGoalAccordionContent({ goalId, studyId, questionBookSubmitted }: Props) {
 	const router = useRouter();
+
 	return (
 		<div>
 			<div className="grid grid-cols-2 gap-2">
-				<DefaultButton
-					className="mt-3"
-					// TO DO : 스터디 번호로 연결
-					onClick={() => router.push(`/studies/${studyId}`)}
-					text="문제집 보러가기"
-				/>
-				{/* TO DO : goal Id 로 연결 */}
-				<DefaultButton className="mt-3" onClick={() => router.push(`/create-quiz/${groupId}`)} text="문제 작성하기" />
+				<DefaultButton className="mt-3" onClick={() => router.push(`/studies/${studyId}`)} text="문제집 보러가기" />
+				{questionBookSubmitted ? (
+					<DefaultButton className="mt-3" type="complete" text="작성완료" />
+				) : (
+					<DefaultButton className="mt-3" onClick={() => router.push(`/create-quiz/${goalId}`)} text="문제 작성하기" />
+				)}
 			</div>
 		</div>
 	);
