9d053e62b700c04d6751b66daeca8d61bec4c10e
chajuhui123 <chajuhui123@naver.com>
2022-11-06 16:19:09 +0900

    feat: 첫 로그인시 추가 정보 입력 화면 라우터 및 스크린 셋팅

---
+++ pages/first-login/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import FirstLoginScreen from '@app.feature/first-login/screen/FirstLoginScreen';
+
+const FirstLogin: NextPage = () => {
+	return <FirstLoginScreen />;
+};
+
+export default FirstLogin;
---
+++ src/app.feature/first-login/screen/FirstLoginScreen.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FirstLoginScreen() {
+	return <>정보입력화면</>;
+}
